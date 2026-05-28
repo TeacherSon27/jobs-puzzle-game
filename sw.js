4b64bf4c95e7e86d910a8210c647593ae5514773
Teacher Son <ericsonsevilla@yahoo.com>
2026-05-28 08:36:42 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,43 @@
-const CACHE_NAME = 'jobs-puzzle-v1';
+const CACHE_NAME = 'jobs-puzzle-v2';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-180.png',
+  './icon-192.png',
+  './icon-512.png'
+];
 
 self.addEventListener('install', event => {
-  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
 });
 
 self.addEventListener('activate', event => {
-  clients.claim();
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+      .then(() => clients.claim())
+  );
 });
 
 self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+    caches.match(event.request).then(cached => {
+      const fetched = fetch(event.request).then(response => {
+        if (response && response.ok) {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        }
+        return response;
+      });
+
+      return cached || fetched.catch(() => cached);
+    })
   );
 });
